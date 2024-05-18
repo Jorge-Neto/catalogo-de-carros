305efc79f5baba84c2c1ba282bc203d8210828e7
Jorge-Neto <neto_tunas@hotmail.com>
2024-05-18 16:26:10 -0300

    Exception handler added

---
+++ src/main/kotlin/tech/developer/catalogodecarros/utils/exceptions/ExceptionHandler.kt
@@ -0,0 +1,22 @@
+package tech.developer.catalogodecarros.utils.exceptions
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.lang.Exception
+
+@RestControllerAdvice
+class ExceptionHandler {
+
+    @ExceptionHandler(Exception::class)
+    fun generalExceptionHandler(exception: Exception): ResponseEntity<ApiError> {
+        val error = ApiError(exception.message)
+        return ResponseEntity(error, error.status)
+    }
+
+    @ExceptionHandler(MakeException::class)
+    fun makeExceptionHandler(exception: Exception): ResponseEntity<ApiError> {
+        val error = ApiError(exception.message)
+        return ResponseEntity(error, error.status)
+    }
+}
---
+++ src/main/kotlin/tech/developer/catalogodecarros/utils/exceptions/MakeException.kt
@@ -0,0 +1,3 @@
+package tech.developer.catalogodecarros.utils.exceptions
+
+class MakeException (override val message:String?): Exception(message)
